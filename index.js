658b35f8e8ce6379dbec5d1bd54888298269f4b9
Zohaib <zohaibmanzoor1700@gmail.com>
2021-11-16 16:03:25 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//write your code here
+const replaceWithYerr = (months, start = 0, end = months.length) => {
+    let myArray = [];
+        for (let i = 0; i <months.length; i++){
+                if(i >= start && i < start + end){
+                    myArray.push("yerr")
+                } 
+                else {
+                    myArray.push(months[i]);
+                }
+        }
+         return myArray;
+    };
+   
+
+console.log(replaceWithYerr(["jan", "feb", "march"]))
